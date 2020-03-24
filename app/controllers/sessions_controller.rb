2994afbdb20c6e232db1f50bc3719b8bc4039baf
Yassin AGX <yassin.agx+git@gmail.com>
2020-03-25 00:04:56 +0100

    Adding sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
     logout if logged_in?
     redirect_to root_path
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(session_params[:email].downcase, session_params[:password])
+      redirect_to root_path
+    else
+      redirect_to new_sessions_path(invalid_session_params: :true)
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password, :invalid_session_params)
+  end
 end
